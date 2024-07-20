e1f5e4864d3bcef20b85fa26e812421186fcdbe2
root <root@localhost.localdomain>
2024-07-20 20:24:14 -0300

    0.2

--- flask_phishing.py
+++ flask_phishing.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request
+from termcolor import colored
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ip=request.remote_addr
-    user_agent=request.headers.get('User-Agent')
+    
+    user_info ={
+            'ip':request.remote_addr,
+            'user_agent':request.headers.get('User-Agent'),
+            'headers':dict(request.headers),
+    }
+
+    print(colored(f"Get request info: {user_info}"))
 
     with open('click_log.txt', 'a') as f:
-        f.write(f"IP: {ip}, User-Agent: {user_agent}\n")
+        f.write(f"{user_info}")
 
     return "This was a phishing simulation. Your click has been logged."
 
